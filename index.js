a8fb5bbff5c29b926e88cd3878581b1f3cfa33b2
cbarange <cbarange.dev@gmail.com>
2021-01-12 16:22:48 +0100

    add api

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ require('dotenv').config()
 
 const express = require('express')
 const morgan = require('morgan')
-
+const axios = require('axios');
+const FormData = require('form-data');
 
 
 const app = express()
@@ -10,8 +11,32 @@ app.enable('trust proxy')
 app.use(morgan('common'))
 app.use(express.json())
 
-app.get('/', async (req, res) => {
-  res.json("HELP")
+app.get('/:number', async (req, res) => {
+  console.log(req.params.number)
+
+  var data = new FormData();
+  data.append('tool', 'nombres-cisterciens');
+  data.append('number', req.params.number);
+
+  var config = {
+    method: 'post',
+    url: 'https://www.dcode.fr/api/',
+    headers: { 
+      'Cookie': 'PHPSESSID=e5b3d1fbee076ebaba4a3cc1c558baa1', 
+      ...data.getHeaders()
+    },
+    data : data
+  };
+
+  axios(config)
+  .then(function (response) {
+    console.log(res.json(response.data));
+  })
+  .catch(function (error) {
+    res.json(error)
+  })
+
+  
 })
 
 const port = process.env.PORT || 5225
